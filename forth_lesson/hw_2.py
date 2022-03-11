74636902feebfce84d1972125ff8854f9053cd05
Bekzhan <abcaazh@gmail.com>
2022-03-11 22:08:30 +0600

    currency_rates

--- forth_lesson/hw_2.py
+++ forth_lesson/hw_2.py
@@ -1,9 +1,32 @@
+from bs4 import BeautifulSoup
 from requests import get
-
-response = get('http://www.cbr.ru/scripts/XML_daily.asp')
-a = response.content
-print(a)
 
 
 def currency_rates(val):
-    return
+    response = get('http://cbr.ru/scripts/XML_daily.asp')
+    src = response.text
+    soup = BeautifulSoup(src, "lxml")
+    currency = []
+
+    num_code = soup.find_all("numcode")
+    char_code = soup.find_all("charcode")
+    name = soup.find_all("name")
+    value = soup.find_all("value")
+
+    i = 0
+    while i < len(name):
+        currency.append({
+            "name": name[i].text,
+            "char_code": char_code[i].text,
+            "value": value[i].text,
+            "num_code": num_code[i].text,
+        })
+        i += 1
+    for item in range(len(currency)):
+        if currency[item]['char_code'] == val:
+            return currency[item]['value']
+        else:
+            print("None")
+
+
+print(f'{currency_rates("AUD")}')
