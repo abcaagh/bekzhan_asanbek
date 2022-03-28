6ef09a093d27b6a7be7e87dc637efb15f7c1728a
Bekzhan <abcaazh@gmail.com>
2022-03-28 22:30:08 +0600

    home work5

--- dz_5/hw_3.py
+++ dz_5/hw_3.py
@@ -4,3 +4,21 @@ tutors = [
 klasses = [
     '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
 ]
+
+
+def turple_gen(tutors, klasses):
+    len_args = len(klasses) - len(tutors)
+
+    if len(tutors) > len(klasses):
+        len_args = len(tutors) - len(klasses)
+        for i in range(len(tutors) - len_args, len(tutors)):
+            klasses.append('None')
+    else:
+        for i in range(len(klasses) - len_args, len(klasses)):
+            tutors.append('None')
+
+    for cell_1, cell_2 in zip(tutors, klasses):
+        yield cell_1, cell_2
+
+
+print(*turple_gen(tutors, klasses))
